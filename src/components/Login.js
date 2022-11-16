8f97ea5c86eb6d6f256762de42ae4fe0f29c9610
Lee Maxwell <maxwellbaboula@gmail.com>
2022-11-17 00:57:21 +0100

    Login auth build

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,46 @@
-import React from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import React, { useRef, useState } from "react";
+import { auth } from "../utils/firebase.config";
 
 const Login = () => {
+  const loginEmail = useRef();
+  const loginPassword = useRef();
+  const [error, setError] = useState(false);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const user = await signInWithEmailAndPassword(
+        auth,
+        loginEmail.current.value,
+        loginPassword.current.value
+      );
+      console.log(user);
+    } catch (error) {
+      console.log(error.message);
+      setError(true);
+    }
+  };
+
   return (
-    <div>
-      <h1>Se connecter</h1>
+    <div className="login-container">
+      <div className="login">
+        <h3>Se connecter</h3>
+        <form className="form-login" onSubmit={(e) => handleLogin(e)}>
+          <input type="email" placeholder="Email" ref={loginEmail} required />
+          <input
+            type="password"
+            placeholder="Mot de passe"
+            ref={loginPassword}
+            required
+          />
+          <input type="submit" value="Se connecter" />
+          <span>
+            {error && `Le mail ou le mot de passe ne correspondent pas`}
+          </span>
+        </form>
+      </div>
     </div>
   );
 };
